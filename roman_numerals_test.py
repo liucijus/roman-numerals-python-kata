6b87ed35328ff99618c2ee783b1db31e72b7047a
Vaidas Pilkauskas <vaidas.pilkauskas@gmail.com>
2016-04-15 15:58:35 +0300

    Refactor to parametrized pytest tests

--- roman_numerals_test.py
+++ roman_numerals_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 def to_roman(number):
     rules = [
             (1000, 'M'), 
@@ -26,28 +28,27 @@ def to_roman(number):
 
     return convert_to_roman(number, rules)
 
-tests = [
-        (1, 'I'),
-        (2, 'II'),
-        (3, 'III'),
-        (4, 'IV'),
-        (5, 'V'),
-        (6, 'VI'),
-        (9, 'IX'),
-        (10, 'X'),
-        (16, 'XVI'),
-        (20, 'XX'),
-        (40, 'XL'),
-        (50, 'L'),
-        (90, 'XC'),
-        (100, 'C'),
-        (400, 'CD'),
-        (500, 'D'),
-        (900, 'CM'),
-        (1000, 'M')
-        ]
+@pytest.mark.parametrize("arabic,roman", [
+    (1, 'I'),
+    (2, 'II'),
+    (3, 'III'),
+    (4, 'IV'),
+    (5, 'V'),
+    (6, 'VI'),
+    (9, 'IX'),
+    (10, 'X'),
+    (16, 'XVI'),
+    (20, 'XX'),
+    (40, 'XL'),
+    (50, 'L'),
+    (90, 'XC'),
+    (100, 'C'),
+    (400, 'CD'),
+    (500, 'D'),
+    (900, 'CM'),
+    (1000, 'M')
+    ])
 
-def test_arabic_to_roman_conversion():
-    for (arabic, roman) in tests:
-        assert to_roman(arabic) == roman
+def test_arabic_to_roman_conversion(arabic, roman):
+    assert to_roman(arabic) == roman
 
